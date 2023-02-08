6f319f3bd31aca8ad2e0dae065dc3919fb1d23bc
sdrartus <stephartus20@gmail.com>
2023-02-08 17:00:05 +0800

    added nltk

--- process_text.py
+++ process_text.py
@@ -1,1 +1,14 @@
 import re as r
+import nltk, pprint
+from nltk import word_tokenize
+
+euph = "C:/Users/Sir/Desktop/Final/Final/euphemism.csv"
+profane = "C:/Users/Sir/Desktop/Final/Final/bad-words.txt"
+euph_infile = open (euph, 'r')
+prof_infile = open(profane, 'r')
+elines = euph_infile.read()
+plines = prof_infile.read()
+
+tokens = word_tokenize(plines)
+
+print(len(tokens))
