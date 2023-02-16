890e820a6f1b7b42503d48fb7f4d398cd6900394
sdrartus <stephartus20@gmail.com>
2023-02-16 20:46:52 +0800

    added the real time nltk

---
+++ realtime.py
@@ -0,0 +1,13 @@
+# remove question marks, special char with re
+import re
+import tkinkter as tik
+from tkinter.srolledtext import ScrolledText
+
+import nltk
+from nltk.corpus import words
+
+nltk.download("words")
+
+class wordChecker:
+    def __init__(self):
+        pass
